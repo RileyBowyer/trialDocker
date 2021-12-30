2f723068e92ca5e3c6f93fcd9524c58a76e5d927
RileyBowyer <riley.d.bowyer@gmail.com>
2021-12-30 14:58:01 +1000

    Removed action

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 docker build -t uqr/base -f Dockerfile-base .
 docker build -t uqr/ws -f Dockerfile-ws . 
+
+# To Transfer Image
+# docker save uqr/ws:latest | bzip2 | pv | ssh user@host docker load
